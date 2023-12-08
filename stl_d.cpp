f0f4aefe6ee7c382d2fe13ef2604fff963da3f7f
Veronika Vronskaya <vveronika1917@gmai.com>
2023-12-08 23:02:52 +0300

    Конечное решение.

--- stl_d.cpp
+++ stl_d.cpp
@@ -6,28 +6,28 @@ using namespace std;
 
 class Sets {
  private:
-        map<int, multiset<long long>> sets;
-        map<long long, multiset<int>> sets_1;
+        map<long long, set<long long>> sets;
+        map<long long, set<long long>> sets_1;
 
  public:
-        void add(int s, long long e) {
+        void add(long long e, long long s) {
             sets[s].insert(e);
             sets_1[e].insert(s);
         }
 
-        void del(int s, long long e) {
-            sets[s].erase(sets[s].find(e));
-            sets_1[e].erase(sets_1[e].find(s));
+        void del(long long e, long long s) {
+            sets[s].erase(e);
+            sets_1[e].erase(s);
         }
 
-        void clear(int s) {
+        void clear(long long s) {
             for (long long i : sets[s]) {
-                sets_1[i].erase(sets_1[i].find(s));
+                sets_1[i].erase(s);
             }
             sets[s].clear();
         }
 
-        void listset(int s) {
+        void listset(long long s) {
             if (sets[s].empty()) {
                 cout << -1;
             } else {
@@ -51,23 +51,22 @@ class Sets {
 };
 
 int main() {
-    long long n, e;
-    int m, k, s;
+    long long n, e, m, k, s;
 
     cin >> n >> m >> k;
 
     Sets sets;
 
-    for (int i = 0; i < k; i++) {
+    for (long long i = 0; i < k; i++) {
         string command;
         cin >> command;
 
         if (command == "ADD") {
             cin >> e >> s;
-            sets.add(s, e);
+            sets.add(e, s);
         } else if (command == "DELETE") {
             cin >> e >> s;
-            sets.del(s, e);
+            sets.del(e, s);
         } else if (command == "CLEAR") {
             cin >> s;
             sets.clear(s);
@@ -81,17 +80,3 @@ int main() {
     }
     return 0;
 }
-
-/*
-9223372036854775807 10
-9
-ADD 1 9
-ADD 1 9
-ADD 1 9223372036854775807
-LISTSET 1
-LISTSETSOF 1
-DELETE 1 9
-LISTSET 2
-CLEAR 1
-LISTSET 1
-*/
