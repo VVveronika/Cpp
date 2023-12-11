ef10e4245fb7a34556d01e8ac306d7aeb14353c1
Veronika Vronskaya <vveronika1917@gmai.com>
2023-12-12 00:01:28 +0300

    Нахождение всех убывающих последовательностей длины k из чисел 1...n. Не успевает по времени.

---
+++ comb_1_e.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void gen(vector<int> pref, int n, int k) {
+    int len = pref.size();
+    if (k == len) {
+        for (int i : pref) {
+            cout << i << " ";
+        }
+        cout << endl;
+        return;
+    }
+    for (int j = 1; j <= n; j++) {
+        if (pref.empty() || j < pref.back()) {
+            pref.push_back(j);
+            gen(pref, j - 1, k);
+            pref.pop_back();
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> pref;
+    gen(pref, n , k);
+    return 0;
+}
