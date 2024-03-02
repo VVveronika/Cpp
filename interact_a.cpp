c63548a1703cc9dfffede75025b280aefd2cfbf2
Veronika Vronskaya <vveronika1917@gmai.com>
2024-03-02 20:10:22 +0300

    Интерактивная задача. Определение простоты числа на основании ответов на запросы.

--- interact_a.cpp
+++ interact_a.cpp
@@ -10,15 +10,27 @@ int main() {
 
     int cnt = 0;
     string answer;
+    int divider = 0;
+
     for (int i : primes) {
         cout << i << endl;
         cin >> answer;
         if (answer == "yes") {
             cnt++;
-            primes.push_back(i * i);
+            divider = i;
+            // primes.push_back(i * i);
         }
 
         if (cnt == 2) {
+            cout << "composite" << endl;
+            return 0;
+        }
+    }
+
+    if (cnt == 1 && divider < 11) {
+        cout << divider * divider << endl;
+        cin >> answer;
+        if (answer == "yes") {
             cout << "composite" << endl;
             return 0;
         }
