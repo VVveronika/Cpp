8783f6d89a88e15471790165677d94f6b64b22b4
Veronika Vronskaya <vveronika1917@gmai.com>
2024-03-22 18:59:55 +0300

    BFS с ограничением. Требует много памяти.

--- short_path_j.cpp
+++ short_path_j.cpp
@@ -12,44 +12,51 @@ struct Neighbour {
 
 struct Vertex {
     long long best_way = INT_MAX;
-    bool in_queue = false;
+    // bool in_queue = false;
     int index;
     int turn = 0;
     vector<Neighbour> neighbours;
 };
 
 
-void bfs (int start_town_index, int max_turn, vector<Vertex>& towns) {
-    queue<int> q;
-    q.push(start_town_index);
-    q.push(-1);
+int bfs(int start_town_index, int end_town_index, int max_turn, vector<Vertex>& towns) {
+    queue<Vertex> q;
+    // q.push(-1);
     Vertex& town = towns[start_town_index];
-    town.in_queue = true;
+    // town.in_queue = true;
     town.best_way = 0;
-    
-    while (!q.empty() && max_turn > 0) {
-        int town_index = q.front();
+    q.push(towns[start_town_index]);
+    Vertex separator;
+    separator.best_way = -1;
+    q.push(separator);
+    int answer = INT_MAX;
+
+    while (!q.empty() && max_turn >= 0) {
+        town = q.front();
         q.pop();
-        if (town_index == -1) {
+        if (town.best_way == -1) {
             max_turn--;
-            q.push(-1);
+            q.push(separator);
             continue;
         }
-        town = towns[town_index];
-        town.in_queue = false;
+        if (town.index == end_town_index && town.best_way < answer) {
+            answer = town.best_way;
+        }
+        // town = towns[town_index];
+        // town.in_queue = false;
         for (auto [neighbour_index, price] : town.neighbours) {
-            Vertex& neighbour = towns[neighbour_index];
+            Vertex neighbour = towns[neighbour_index];
             if (town.best_way + price < neighbour.best_way) {
                 neighbour.best_way = town.best_way + price;
                 neighbour.turn = town.turn + 1;
-                if (!neighbour.in_queue) {
-                    q.push(neighbour_index);
-                    neighbour.in_queue = true;
-                }
+                // if (!neighbour.in_queue) {
+                    // neighbour.in_queue = true;
+                q.push(neighbour);
+                // }
             }
         }
     }
-
+    return answer;
 }
 
 
@@ -73,12 +80,20 @@ int main() {
         towns[to_vertex].index = to_vertex;
     }
 
-    bfs(from_town_index, k, towns);
+    int answer = bfs(from_town_index, to_town_index, k, towns);
 
-    int answer = -1;
-    if (towns[to_town_index].turn <= k) {
-        answer = towns[to_town_index].best_way;
-    }
+    answer = answer == INT_MAX ? -1 : answer;
+    // if (towns[to_town_index].turn <= k) {
+    //     answer = towns[to_town_index].best_way;
+    // }
     cout << answer;
     return 0;
 }
+/*
+4 5 2 1 4
+1 2 1
+2 3 1
+3 4 1
+1 3 3
+1 4 5
+*/
